803fc09fc2f18ee8b954fa9435bdabbdddfb75b4
umeshjoshi94 <josshiumesh.90@gmail.com>
2023-02-01 16:12:18 +0530

    some file based routing
    using next for file based routing through components

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,8 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm"
+const NewMeetUpPage=()=>{
+    const addMeetupHandler=(enteredMeetupData)=>{
+        console.log(enteredMeetupData)
+    }
+    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+}
+ export default NewMeetUpPage;
