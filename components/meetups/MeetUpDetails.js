7df7feb717ed1b2a0d56638f1b76b8e3411eca24
umeshjoshi94 <josshiumesh.90@gmail.com>
2023-02-03 16:10:30 +0530

    adding custom componnent(Detail page)

---
+++ components/meetups/MeetUpDetails.js
@@ -0,0 +1,12 @@
+import classes from './MeetUpDetails.module.css'
+
+const MeetUpDetail=(props)=>{
+    return <section className={classes.detail}>
+        <img src={props.image} alt={props.title}/>
+        <h1>{props.title}</h1>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+    </section>
+}
+
+export default MeetUpDetail;
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,10 @@
+import MeetUpDetail from "../../components/meetups/MeetUpDetails"
+const MeetUpDetails=()=>{
+    return <MeetUpDetail
+    image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
+    title='A first meetup'
+    address='some street 5'
+    description='This is a first meetup'
+    />
+}
+export default MeetUpDetails
